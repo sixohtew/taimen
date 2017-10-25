4fa1894429b41fb0fb9cb3ccb49f2b5850295b88
flar2 <asegaert@gmail.com>
2017-10-24 23:37:19 -0400

    taimen: allow stock modules to load
    Signed-off-by: Joe Maples <joe@frap129.org>

--- kernel/module.c
+++ kernel/module.c
@@ -1295,6 +1295,18 @@ static int check_version(Elf_Shdr *sechdrs,
 	unsigned int i, num_versions;
 	struct modversion_info *versions;
 
+	if(!strncmp("touch_core_base", mod->name, 15))
+		return 1;
+
+	if(!strncmp("ftm4", mod->name, 4))
+		return 1;
+
+	if(!strncmp("sw49408", mod->name, 7))
+		return 1;
+
+	if(!strncmp("lge_battery", mod->name, 11))
+		return 1;
+
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
 		return 1;
